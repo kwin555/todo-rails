9a1680395b6cb8a16cce4b64c5312d8677a1ee23
kwin555 <ktn555@att.net>
2018-03-01 23:07:34 -0600

    added toggle

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def index
     render json: Task.order(:id)
   end
@@ -8,6 +8,7 @@ class TasksController < ApplicationController
   def update
     task = Task.find(params[:id])
     task.update_attributes(task_params)
+    render json: task
   end
 
   def create
